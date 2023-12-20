bd4ec65c2a2eb9d3a4a9bf0ccc90f68e092e79ad
liuxinrui5257 <xxl180033@utdallas.edu>
2023-12-20 01:51:00 -0600

    4C init

---
+++ wp-content/themes/wp-dev/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body <?php body_class(); ?>>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, placeat ad pariatur modi reiciendis tempore
+        inventore aliquam odio sapiente nulla dolor ipsa iure a unde. Tempore id repellat sunt maiores?</p>
+    <div id="page" class="class">
+        <header>
+            <section class="top-bar">
+                <div class="logo">Logo</div>
+                <div class="searchbox"></div>
+            </section>
+            <section class="menu-area">
+                <nav class="main">
+
+                </nav>
+            </section>
+        </header>
+        <!-- -->
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <section class="hero">
+
+                    </section>
+                    <section class="services">
+
+                    </section>
+                    <section class="home-blog">
+
+                    </section>
+                </main>
+            </div>
+        </div>
+        <!-- -->
+
+        <footer class="site-footer"></footer>
+    </div>
+
+</body>
+
+</html>
